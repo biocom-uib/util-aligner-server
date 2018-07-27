e704c4b6dfcfd12e40c0251c1201747be63e24e5
Projecte PINAWeb <pinaweb@ltimbigd2.uib.es>
2018-07-27 11:48:05 +0200

    reorganize container users, ports, etc

---
+++ mysql-initdb.d/create-users.sql
@@ -0,0 +1,15 @@
+create user if not exists 'util-aligner-admin'@'127.0.0.1' identified by 'util-aligner-admin';
+grant all on protein_db.* to 'util-aligner-admin'@'127.0.0.1';
+
+create user if not exists 'util-aligner-updater'@'172.20.0.1' identified by 'util-aligner-updater';
+grant all on protein_db.* to 'util-aligner-updater'@'172.20.0.1';
+
+create user if not exists 'util-aligner-server'@'172.20.0.2' identified by 'util-aligner-server';
+grant all on protein_db.* to 'util-aligner-server'@'172.20.0.2';
+
+create user if not exists 'util-aligner-api'@'%' identified by 'util-aligner-api';
+grant select on protein_db.* to 'util-aligner-api'@'%';
+
+delete from mysql.user where user='root' and host not in ('localhost', '127.0.0.1', '::1');
+
+flush privileges;
