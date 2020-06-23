biocom-uib/util-aligner-server

commit 3929c6e1208c3670f7b8d1da8cad42def2fc0ab7

Output fixes and cleanup Move more dataframe-like code to pandas

server/scores/functional.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from math import isnan
+import pandas as pd
 
 from go_tools import init_default_hrss
 from semantic_similarity import JaccardSim
@@ -28,7 +29,7 @@ def compute_fc(alignment, ontology_mapping, dissim):
 
     results = []
 
-    for p1_name, p2_name in alignment.itertuples(name=None):
+    for p1_name, p2_name in alignment.itertuples(name=True):
         gos1 = frozenset(ontology_mapping.get(p1_name, []))
         gos2 = frozenset(ontology_mapping.get(p2_name, []))
 
@@ -40,7 +41,8 @@ def compute_fc(alignment, ontology_mapping, dissim):
             fc_len += 1
 
     fc_avg = fc_sum/fc_len if fc_len > 0 else -1
-    return results, fc_avg
+    results_df = pd.DataFrame(results, columns=[alignment.index.name, alignment.columns[0], 'fc'])
+    return results_df, fc_avg
 
 
 def compute_bitscore_fc(alignment, bitscore_matrix):
@@ -75,8 +77,8 @@ def compute_fc_scores(net1, net2, alignment, bitscore_matrix, ontology_mapping):
             'fc_values_jaccard': fc_values_jaccard,
             'fc_score_hrss_bma': fc_hrss_bma,
             'fc_values_hrss_bma': fc_values_hrss_bma,
-            'unannotated_prots_net1': list(no_go_prots_net1),
-            'unannotated_prots_net2': list(no_go_prots_net2),
+            'unannotated_prots_net1': pd.Series(list(no_go_prots_net1), name='unannotated_prots_net1'),
+            'unannotated_prots_net2': pd.Series(list(no_go_prots_net2), name='unannotated_prots_net2'),
             'ann_freqs_net1': {str(ann_cnt): freq for ann_cnt, freq in ann_freqs_net1.items()},
             'ann_freqs_net2': {str(ann_cnt): freq for ann_cnt, freq in ann_freqs_net2.items()}
         })

server/main.py
@@ -131,9 +131,9 @@ async def process_alignment(job_id, data):
 
     if 'alignment' in results:
         alignment = results['alignment']
-        results['alignment'] = None
+        results['alignment'] = {'file': 'alignment_tsv'}
 
-        result_files['alignment_tsv'] = write_tsv_to_string(alignment)
+        result_files['alignment_tsv'] = write_tsv_to_string(alignment.reset_index())
 
         logger.info(f'[{job_id}] computing scores')
 
@@ -183,6 +183,9 @@ async def fetch_and_validate_previous_results(job_id, result_ids):
     records = await gather(*[retrieve_alignment_result(result_id) for result_id in result_ids])
     records = [record for record in records if record['results']['ok'] and 'alignment_tsv' in record['files']]
 
+    if len(records) == 0:
+        raise ValueError(f'[{job_id}] no successful alignments to compare')
+
     db_names = [record['db'].lower() for record in records]
     if not all_equal(db_names):
         raise ValueError(f'[{job_id}] mismatching databases: ' + str(db_names))
@@ -212,7 +215,9 @@ async def fetch_and_validate_previous_results(job_id, result_ids):
     for aligner, alignment in zip(aligners, alignments):
         alignment.rename(inplace=True, columns=lambda col: f'{col}_{aligner}')
 
-    joined = alignments[0].join(alignments[1:], how='outer')
+    # TODO start with Series(net1.vs['name'], name=alignments[0].index.name)
+
+    joined = alignments[0].join(alignments[1:], how='outer').rename_axis(index=alignments[0].index.name).reset_index()
 
     return db_names[0], net1_descs[0], net2_descs[0], records, alignment_headers[0], joined
 
@@ -223,7 +228,7 @@ async def compare_alignments(job_id, data):
     results = {'ok': True, 'exception': None}
     result_files = dict()
 
-    result_ids = data['results_object_ids']
+    result_ids = data.get('results_object_ids', [])
 
     response_data = {'results': results, 'results_object_ids': result_ids}
 
@@ -245,12 +250,12 @@ async def compare_alignments(job_id, data):
 
     else:
         consensus = joined.loc[joined.agg(all_equal, axis=1)].iloc[:,:1]
-        consensus.rename_axis(alignment_header[0], inplace=True)
         consensus.columns = [alignment_header[1]]
+        consensus.rename_axis(index=alignment_header[0], inplace=True)
 
         results.update({
-            'joined': None,
-            'consensus': None
+            'joined': {'file': 'joined_tsv'},
+            'consensus': {'file': 'consensus_tsv'}
         })
 
         result_files.update({

server/mongo.py
@@ -21,7 +21,8 @@ async def insert_split(collection, job_id, document, files=dict()):
     file_ids = dict()
 
     for filename, content in files.items():
-        file_ids[filename] = await gridfs.upload_from_stream(f'{job_id}/{filename}', content.encode('utf-8'))
+        file_id = await gridfs.upload_from_stream(f'{job_id}/{filename}', content.encode('utf-8'))
+        file_ids[filename] = str(file_id)
 
     document['files'] = file_ids
 

server/scores/__init__.py
@@ -34,7 +34,7 @@ def split_score_data_as_tsvs(scores):
             tsv_key = '/'.join(path) + '_tsv'
 
             tsvs[tsv_key] = write_tsv_to_string(parent_dict[path[-1]])
-            parent_dict[path[-1]] = None
+            parent_dict[path[-1]] = {'file': tsv_key}
 
     key_to_file('ec_data', 'invalid_images')
     key_to_file('ec_data', 'unaligned_nodes')

server/scores/topology.py
@@ -39,33 +39,27 @@ def compute_ec_image_scores(net1, net2, alignment):
 
     num_preserved_edges = int(preserved_edges.is_edge.sum())
 
-    non_preserved_edges = list(
-        preserved_edges.reset_index()
-            .loc[lambda df: ~df.is_edge, ['source_orig', 'target_orig']]
-            .astype(str)
-            .itertuples(index=False, name=None)
-    )
+    non_preserved_edges = preserved_edges.reset_index() \
+        .loc[lambda df: ~df.is_edge, ['source_orig', 'target_orig']] \
+        .astype(str)
 
-    unaligned_edges = list(
-        image.loc[image.source.isna() | image.target.isna(), ['source_orig', 'target_orig']]
-            .astype(str)
-            .itertuples(index=False, name=None)
-    )
+    unaligned_edges = image.loc[image.source.isna() | image.target.isna(), ['source_orig', 'target_orig']] \
+        .astype(str)
 
     min_es = net1.igraph.ecount() if net1.igraph.vcount() <= net2.igraph.vcount() else net2.igraph.ecount()
 
     return {
-        'invalid_images': invalid_images,
+        'invalid_images': pd.Series(invalid_images, name='invalid_images'),
         'num_invalid_images': len(invalid_images),
 
-        'unaligned_nodes': unaligned_nodes,
+        'unaligned_nodes': pd.Series(unaligned_nodes, name='unaligned_nodes'),
         'num_unaligned_nodes': len(unaligned_nodes),
 
         'unaligned_edges': unaligned_edges,
         'num_unaligned_edges': len(unaligned_edges),
 
-        'num_preserved_edges': num_preserved_edges,
         'non_preserved_edges': non_preserved_edges,
+        'num_preserved_edges': num_preserved_edges,
 
         'min_n_edges': min_es,
         'ec_score': num_preserved_edges/min_es if min_es > 0 else -1.0,
@@ -89,16 +83,13 @@ def compute_ec_preimage_scores(net1, net2, alignment):
 
     num_reflected_edges = int(reflected_edges.is_edge.sum())
 
-    non_reflected_edges = list(
-        reflected_edges.reset_index() \
-            .loc[lambda df: ~df.is_edge, ['source_orig', 'target_orig']] \
-            .astype(str)
-            .itertuples(name=None)
-    )
+    non_reflected_edges = reflected_edges.reset_index() \
+        .loc[lambda df: ~df.is_edge, ['source_orig', 'target_orig']] \
+        .astype(str)
 
     return {
+        'non_reflected_edges': non_reflected_edges,
         'num_reflected_edges': num_reflected_edges,
-        'non_reflected_edges': non_reflected_edges
     }
 
 

server/util.py
@@ -44,7 +44,10 @@ def write_csv(file_path, records, **kwargs):
 
 def write_tsv_to_string(table, header=[]):
     if isinstance(table, pd.DataFrame):
-        return table.to_csv(sep='\t', header = header or True)
+        return table.to_csv(sep='\t', index=False, header = header or True)
+
+    elif isinstance(table, pd.Series):
+        return table.to_csv(sep='\t', index=False, header = header or True)
 
     else:
         f = StringIO()

commit c7b6851a02b884a652a3a56b85a3a921e7c9c689

fix scores/functional.py bug introduced in 3929c6e1208c3670f7b8

server/scores/functional.py
@@ -29,7 +29,7 @@ def compute_fc(alignment, ontology_mapping, dissim):
 
     results = []
 
-    for p1_name, p2_name in alignment.itertuples(name=True):
+    for p1_name, p2_name in alignment.itertuples(names=None, index=True):
         gos1 = frozenset(ontology_mapping.get(p1_name, []))
         gos2 = frozenset(ontology_mapping.get(p2_name, []))
 